d2c0b3b473d17074788f3c39f1b19eb100b490a3
Raí <megac154@gmail.com>
2023-09-22 14:17:05 -0300

    filter in agend ok

---
+++ src/state/seletores/index.ts
@@ -0,0 +1,25 @@
+import { selector } from "recoil";
+import { filtroDeEventos, listaDeEventosState } from "../atom";
+
+export const eventosFiltradosState = selector({
+  key: "eventosFiltradosState",
+  get: ({ get }) => {
+    const filtro = get(filtroDeEventos)
+    const todosOsEventos = get(listaDeEventosState)
+
+    const eventos = todosOsEventos.filter((evento) => {
+      if (!filtro.data) {
+        return true
+      }
+
+      // "2023-09-22"
+      const ehOMesmoDia =
+        filtro.data.toISOString().slice(0, 10) ===
+        evento.inicio.toISOString().slice(0, 10)
+      return ehOMesmoDia
+    })
+    return eventos
+  }
+})
+
+
